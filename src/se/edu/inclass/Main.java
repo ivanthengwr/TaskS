044b5cbb2b4a13f9b5bccec9f1e082ed97c9499a
ivanthengwr <ivanthengwr@gmail.com>
2022-09-16 17:48:58 +0800

    Update lambda function

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -5,7 +5,11 @@ import se.edu.inclass.task.Deadline;
 import se.edu.inclass.task.Task;
 import se.edu.inclass.task.TaskNameComparator;
 
+import javax.sound.midi.Soundbank;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+
+import static java.util.stream.Collectors.toList;
 
 public class Main {
 
@@ -17,9 +21,13 @@ public class Main {
 
 //        System.out.println("All data: ");
 //        printData(tasksData);
-//        System.out.println("Printing deadlines");
+//        System.out.println("Printing deadlines loop");
 //        printDeadlines(tasksData);
+//        printDeadlinesUsingStream(tasksData);
 
+        ArrayList<Task> filteredList = filterTaskByString(tasksData, "11");
+        System.out.println("Data with string 11:");
+        printData(filteredList);
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
     }
@@ -46,5 +54,22 @@ public class Main {
                 System.out.println(t);
             }
         }
+    }
+    public static void printDeadlinesUsingStream(ArrayList<Task> tasks) {
+        System.out.println("Printing sorted delays");
+        tasks.stream()
+                .filter(t -> t instanceof Deadline)
+                .sorted((a,b)-> a.getDescription()
+                        .compareToIgnoreCase(b.getDescription()))
+                .forEach(System.out::println);
+    }
+
+    public static ArrayList<Task> filterTaskByString(ArrayList<Task> tasks, String filterString) {
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream();
+
+        tasks.stream()
+                .filter(t -> t.getDescription().contains(filterString))
+                .collect(toList());
+        return filteredList;
     }
 }
